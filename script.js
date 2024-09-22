6c4e0e2cb394bcecb39fbd27ec8b21ea7774c679
xjosob <jocke.s@windowslive.com>
2024-09-22 20:37:33 +0200

    added option to save theme preference to local storage

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const themeToggle = document.getElementById("theme-toggle");
+const savedTheme = localStorage.getItem("theme") || "light-theme";
 const body = document.body;
+
+body.classList.add(savedTheme);
+
+themeToggle.checked = savedTheme === "dark-theme";
 
 themeToggle.addEventListener("change", () => {
   if (themeToggle.checked) {
     body.classList.add("dark-theme");
     body.classList.remove("light-theme");
+    localStorage.setItem("theme", "dark-theme");
   } else {
     body.classList.add("light-theme");
     body.classList.remove("dark-theme");
+    localStorage.setItem("theme", "light-theme");
   }
 });
